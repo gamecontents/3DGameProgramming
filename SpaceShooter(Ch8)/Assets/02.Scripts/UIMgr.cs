975fda95e9d69a958610599ac47639821a6480e4
gamecontents <gamecontents@naver.com>
2018-05-31 23:27:21 +0900

    Revert "Revert "ch8""
    This reverts commit a36b202b5306b37d065f23a921fa424761830a67.

---
+++ SpaceShooter(Ch8)/Assets/02.Scripts/UIMgr.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIMgr : MonoBehaviour {
+
+    //public void OnClickStartButton(string msg)
+    public void OnClickStartButton(RectTransform rt)
+    {
+        Debug.Log("Scale X: " + rt.localScale.x.ToString());
+    }
+}
